50623ca9d116a8c7a916a880915c0a3fc77817e1
nguyenhuuduc3888 <102501130+nguyenhuuduc3888@users.noreply.github.com>
2022-06-13 10:46:07 +0700

    up

---
+++ ss1/bai_tap/bai_tap_1.sql
@@ -0,0 +1,10 @@
+create database student_management ;
+use student_management;
+
+create table class(class
+id int primary key auto_increment,
+`name` varchar (50));
+
+create table teacher(
+ id int primary key auto_increment,`name` varchar(50),age int,contry varchar (50)
+ );
