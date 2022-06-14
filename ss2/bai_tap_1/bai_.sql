b5c3e5fbd573ef151b6cfc72bda8972365ef384b
nguyenhuuduc3888 <102501130+nguyenhuuduc3888@users.noreply.github.com>
2022-06-14 11:10:30 +0700

    up bai 1

---
+++ ss2/bai_tap_1/bai_.sql
@@ -0,0 +1,69 @@
+create database if not exists chuyen_doi_erd;
+
+use chuyen_doi_erd;
+
+create table phieu_xuat(
+so_san_pham int primary key,
+ngay_san_xuat int 
+);
+
+create table vat_tu(
+ma_vat_tu int primary key,
+ten_vat_tu varchar(100)
+);
+
+create table chi_tiet_phieu_xuat(
+so_san_pham int ,
+ma_vat_tu int,
+don_gia_san_xuat int,
+so_luong_san_xuat int,
+ PRIMARY KEY (so_san_pham ,ma_vat_tu),
+ FOREIGN KEY(so_san_pham) REFERENCES phieu_xuat(so_san_pham),
+ FOREIGN KEY(ma_vat_tu) REFERENCES vat_tu(ma_vat_tu)
+);
+
+create table don_dh(
+so_dh int primary key,
+ngay_dh int
+);
+
+create table chi_tiet_don_dat_hang(
+ma_vat_tu int ,
+so_dh int,
+primary key(ma_vat_tu,so_dh),
+foreign key(ma_vat_tu) references vat_tu(ma_vat_tu),
+foreign key(so_dh) references don_dh(so_dh)
+);
+
+create table sdt(
+sdt int,
+ma_ncc int primary key
+);
+
+create table nha_cung_cap(
+so_dh int ,
+ma_ncc int,
+ten_ncc varchar(50),
+dia_chi varchar(50),
+primary key(ma_ncc),
+id_sdt int,
+foreign key(so_dh) references don_dh(so_dh),
+foreign key(id_sdt) references sdt(ma_ncc)
+);
+
+create table phieu_nhap(
+so_pn int primary key,
+ngay_nhap varchar (50)
+);
+
+create table chi_tiet_phieu_nhap(
+so_pn int,
+ma_vat_tu int,
+dg_nhap int,
+sl_nhap int,
+primary key (so_pn,ma_vat_tu),
+foreign key(so_pn)references phieu_nhap(so_pn),
+foreign key(ma_vat_tu)references vat_tu(ma_vat_tu)
+);
+
+
