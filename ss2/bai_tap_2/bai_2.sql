b2f832fea49e4ea73ef41fbde3e484476ec21dee
nguyenhuuduc3888 <102501130+nguyenhuuduc3888@users.noreply.github.com>
2022-06-14 11:10:58 +0700

    up bai 2

---
+++ ss2/bai_tap_2/bai_2.sql
@@ -0,0 +1,32 @@
+create database if not exists quan_ly_ban_hang;
+
+use quan_ly_ban_hang;
+
+create table customer(
+c_id int primary key,
+c_name varchar(50),
+c_age int
+);
+
+create table `order`(
+o_id int primary key,
+o_date date,
+o_total_price int,
+c_id int,
+foreign key(c_id)references customer(c_id)
+);
+
+create table product(
+p_id int primary key,
+p_name varchar(50),
+p_price int
+);
+
+create table  orderdetail(
+o_id int,
+p_id int,
+od_qty int,
+primary key(o_id,p_id),
+foreign key(o_id)references `order`(o_id),
+foreign key(p_id)references product(p_id)
+);
